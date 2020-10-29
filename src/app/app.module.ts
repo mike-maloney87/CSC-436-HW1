119a40815a372d576d89f219fbb627ee795637e1
mike-maloney87 <71612972+mike-maloney87@users.noreply.github.com>
2020-10-28 19:31:06 -0500

    turn in for hw 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
+import { RouterModule,Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NumberTravelersComponent } from './number-travelers/number-travelers.component';
 import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './about/about.component';
 
 
+const routes: Routes = [
+  {path: 'about', component:AboutComponent},
+  {path: 'travelers',component:NumberTravelersComponent}
+
+]
 @NgModule({
   declarations: [
     AppComponent,
     NumberTravelersComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
